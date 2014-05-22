e1448c2a98d190fc415610af95ce20e4bda45f71
Loy <loyenrique1@gmail.com>
2014-05-22 05:35:36 +0100

    testing basic email sending

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 class IndexController extends AppController {
 	
-	public $components = array('Session');
+	public $components = array('Session', 'Email');
 	public $uses = array('Contacto'); // models
 	
 	public function index(){
@@ -11,6 +11,10 @@ class IndexController extends AppController {
 		if(!empty($this->data)){
 			$saved = $this->Contacto->save($this->data);
 			if($saved){
+				$this->Email->from    = 'alguien@ejemplo.com';
+				$this->Email->to      = 'loyenrique1@gmail.com';
+				$this->Email->subject = 'Prueba';
+				$this->Email->send('Hola cuerpo de mensaje!!!');
 				echo $this->Session->setFlash('Thank you for contacting, you will receive a response within 24 hours.', 'success');
 			}
 		}
