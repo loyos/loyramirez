721ac89d7f26fcb429881c4dbf9184e05324bca9
Loy <loyenrique1@gmail.com>
2014-07-03 01:24:25 +0100

    contact fields validated

--- app/Model/Contacto.php
+++ app/Model/Contacto.php
@@ -2,6 +2,26 @@
 
 class Contacto extends AppModel {
     var $name = 'Contacto';
+
+
+	var $validate = array(
+       
+		'name' => array(
+			'rule' => 'notEmpty',
+			'message' => "This field cannot be empty."
+		),
+		'email' => 'email',
+		'subject' => array(
+			'rule' => 'notEmpty',
+			'message' => 'This field cannot be empty.'
+		),
+		'message' => array(
+			'rule' => 'notEmpty',
+			'message' => 'This field cannot be empty.'
+		)
+    );
+
+	
 }
 
 ?>
